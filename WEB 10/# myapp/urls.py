4b8117a9681896de614000864f0945a8b8941821
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-03-13 22:10:49 +0200

    Delete WEB 10/# myapp directory

--- WEB 10/# myapp/urls.py
+++
@@ -1,8 +0,0 @@
-rom django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_page, name='login'),
-    path('logout/', views.logout_page, name='logout'),
-    path('register/', views.register_page, name='register'),
-]

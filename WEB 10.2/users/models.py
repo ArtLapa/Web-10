63a56f50cecf4f8eef811443a989f3daf305a443
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-04-08 22:27:44 +0300

    Delete WEB 10.2 directory

--- WEB 10.2/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Додайте додаткові поля профілю, якщо потрібно
-
-    def __str__(self):
-        return self.user.username
--- WEB 10.2/users/tests.py
+++
@@ -1,3 +0,0 @@
-from django.test import TestCase
-
-# Тут можуть бути тести для вашого додатку
